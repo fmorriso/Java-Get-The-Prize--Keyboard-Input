faa00c7e63d4b33d190ea08e37ff6f3783279547
Frederick Morrison <frederick.morrison@outlook.com>
2024-07-13 11:01:00 -0600

    extract Key class code into separate file

--- PrizePanel.java
+++ PrizePanel.java
@@ -56,7 +56,7 @@ public class PrizePanel extends JPanel {
         addMouseListener(new Mouse());
 
         // Add a "Listener" for the keyboard controls
-        addKeyListener(new Key());
+        addKeyListener(new Key(FRAME, pd));
         setFocusable(true);
     }
 
@@ -154,63 +154,5 @@ public class PrizePanel extends JPanel {
         }
     }
 
-    private class Key extends KeyAdapter {
-        public void keyPressed(KeyEvent e) {
-            final int increment = FRAME / 25;
-            final double polkaDotRadius = pd.getRadius();
 
-            final int whichKeyWasPressed = e.getKeyCode();
-            switch (whichKeyWasPressed) {
-                // Up arrow key - move the polkadot up towards the top of the screen
-                case KeyEvent.VK_UP:
-                case KeyEvent.VK_W:
-
-                    pd.setY(pd.getY() - increment);
-
-                    // ensure polkadot doesn't go past the top of the screen
-                    if (pd.getY() <= polkaDotRadius) pd.setY(polkaDotRadius);
-
-                    break;
-
-                // Down arrow key - move the polkadot towards the bottom of the screen
-                case KeyEvent.VK_DOWN:
-                case KeyEvent.VK_Z:
-
-                    pd.setY(pd.getY() + increment);
-
-                    // ensure polkadot doesn't go past the bottom of the screen
-                    if (pd.getY() >= (FRAME - polkaDotRadius)) pd.setY(FRAME - polkaDotRadius);
-
-                    break;
-
-                // Left arrow key - move the polkadot towards the left side of the screen
-                case KeyEvent.VK_LEFT:
-                case KeyEvent.VK_A:
-
-                    pd.setX(pd.getX() - increment);
-
-                    // ensure polkadot doesn't go past the left side of the screen
-                    if (pd.getX() <= polkaDotRadius) pd.setX(polkaDotRadius);
-
-                    break;
-
-                // Right arrow key - move the polkadot towards the right side of the screen
-                case KeyEvent.VK_RIGHT:
-                case KeyEvent.VK_S:
-
-                    pd.setX(pd.getX() + increment);
-
-                    // ensure polkadot doesn't go past the right side of the screen
-                    if (pd.getX() >= FRAME - polkaDotRadius) pd.setX(FRAME - polkaDotRadius);
-
-                    break;
-
-                default:
-                    // ignore any other keys that are pressed
-                    break;
-            }
-
-        }
-
-    }
 }
